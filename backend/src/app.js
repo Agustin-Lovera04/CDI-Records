5367ebe640c05883ad88e777b71de8181c5bc4d6
Agustin Lovera <agusslovera04@gmail.com>
2025-09-23 11:32:35 -0300

    Implemenmt router

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,15 @@
 import express from 'express'
 import { config } from './config/config.js'
+import { router as userRouter } from './router/user-router.js'
+import { router as cluesRouter } from './router/clues-router.js'
+import { router as álbumesRouter } from './router/álbumes-router.js'
 
 const app = express()
 const PORT = config.PORT
+
+app.use('/user', userRouter)
+app.use('/clues', cluesRouter)
+app.use('/álbumes', álbumesRouter)
 
 app.listen(PORT, ()=> {
     console.log(`Server online in port ${PORT}`)
